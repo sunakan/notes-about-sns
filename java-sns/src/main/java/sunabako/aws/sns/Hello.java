1574388a3570710a0274d47b5028c2933f1dabf3
sunakan <sunakan03@gmail.com>
2019-10-27 10:24:59 +0900

    [Add] IntelliJ + Gradle5 + JUnit5でHelloWorldする

---
+++ java-sns/src/main/java/sunabako/aws/sns/Hello.java
@@ -0,0 +1,7 @@
+package sunabako.aws.sns;
+
+public class Hello {
+    public static void main(String... args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ java-sns/src/test/java/sunabako/aws/sns/HelloTest.java
@@ -0,0 +1,13 @@
+package sunabako.aws.sns;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HelloTest {
+    @Test
+    void helloTest() {
+        System.out.println("HelloWorld!!");
+        assertEquals("HelloWorld", "HelloWorld");
+    }
+}
